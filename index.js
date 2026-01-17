71d9dd14430e9fb568c9fcca4f4d29908ef6190d
Luis200410 <162214899+Luis200410@users.noreply.github.com>
2026-01-17 08:05:18 -0500

    get the data from the API and updated style

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const form = document.getElementById('form');
+const colorContainer = document.getElementById('color-scheme');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault()
-    const seedColor = document.getElementById('seed-color').value
-    console.log(seedColor)
-})
+    const seedColor = document.getElementById('seed-color').value.slice(1)
+    const schemeMode = document.getElementById('options').value
 
-
-
-
-fetch(' https://www.thecolorapi.com/scheme', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify()
-})
+    fetch(` https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${schemeMode}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify()
+        })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        data.colors.forEach(color =>{
+            console.log(color.hex.value)
+        })
+        
+    })   
+    
+})
 
 
-fetch()
+
+
+
+
+
+
+
