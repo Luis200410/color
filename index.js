9ab9dac240a80d57b520171e7eb86bc07b88b53c
Luis200410 <162214899+Luis200410@users.noreply.github.com>
2026-01-17 21:09:08 -0500

    finish regular goals and added same style as the figma design

--- index.js
+++ index.js
@@ -2,32 +2,28 @@ const form = document.getElementById('form');
 const colorContainer = document.getElementById('color-scheme');
 
 form.addEventListener('submit', function (e) {
+    let colorsArray = []
     e.preventDefault()
     const seedColor = document.getElementById('seed-color').value.slice(1)
     const schemeMode = document.getElementById('options').value
 
-    fetch(` https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${schemeMode}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify()
-        })
+fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${schemeMode}`)
     .then(res => res.json())
     .then(data => {
+        let html = ''
         data.colors.forEach(color =>{
-            console.log(color.hex.value)
+            const colorHex = color.hex.value
+            const colorImage = color.image.bare
+            html += `
+            <div class="color-box">
+                <img src="${colorImage}" alt="color ${colorHex}"/>
+                <div class="code-container">
+                    <p class="color-code">${colorHex}</p>
+                </div>
+            </div> 
+            `
         })
-        
+        colorContainer.innerHTML = html
     })   
-    
-})
 
-
-
-
-
-
-
-
-
+}) 
